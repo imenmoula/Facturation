d5dee62ff5d2a0924c078ed96d64694c839946c6
Moula imen <imenmoula8@gmail.com>
2023-11-26 23:13:25 +0100

    home v0

--- views/home.php
+++ views/home.php
@@ -1,5 +1,52 @@
-<?php
-session_start(); // Démarrer la session sur la page du tableau de bord
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Connexion réussie pour l'utilisateur : " . $_SESSION['name'] ;
-?>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - Partie Admin</title>
+    <!-- Add links to Materialize CSS files and your custom styles here -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Add your custom styles here -->
+    <style>
+        /* Custom styles for the Navbar */
+        nav {
+            background-color: #2196F3; /* Blue background color */
+        }
+
+        /* Customize the brand-logo */
+        .brand-logo {
+            margin-left: 15px; /* Adjust the margin as needed */
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Facturation  <?php echo $_SESSION['name']; ?></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Liste des factures</a></li>
+                <li><a href="#">Liste des clients</a></li>
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Main content -->
+    <div class="container">
+        
+    </div>
+
+    <!-- Add links to Materialize JavaScript files and your custom scripts here -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <!-- Add your custom scripts here -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            // Initialize Materialize components if needed
+        });
+    </script>
+</body>
+
+</html>
